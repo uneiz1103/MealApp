4099eb4de37c7f962916dd9434a855535df47364
uneiz1103 <uneizshaikh1103@gmail.com>
2024-01-20 02:45:16 +0530

    First commit

---
+++ lib/screens/meals.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:meal/models/meal.dart';
+
+class MealScreen extends StatelessWidget {
+  const MealScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("")),
+    );
+  }
+}
